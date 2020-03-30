60d85e2984158caecaf296faae2171ec352d8261
iquery-001 <kota@vaga.tk>
2020-03-30 13:53:31 -0500

    Done.

--- .history/app/application_20200330135151.rb
+++ .history/app/application_20200330135151.rb
@@ -7,14 +7,10 @@ class Application
         if req.path.match(/items/)
             item_name = req.path.split("/items/").last
             
-            if item = @@items.find{|i| i.name == item_name}
-            # binding.pry
-                resp.write item.price
-            else
-                resp.write "Item not found"
-                resp.status = 400
-            end
-
+            resp.status = 200 if item = @@items.find{|i| i.name == item_name}
+            binding.pry
+            resp.write item.price
+            
         else
             resp.write "Route not found"
             resp.status = 404
--- .history/app/application_20200330135240.rb
+++ .history/app/application_20200330135240.rb
@@ -11,10 +11,10 @@ class Application
             # binding.pry
                 resp.write item.price
             else
-                resp.write "Item not found"
                 resp.status = 400
             end
-
+        
+            
         else
             resp.write "Route not found"
             resp.status = 404
